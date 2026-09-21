a408ccbeb057efcd6ac4aa04f4370de0d03bf693
Dhanya <dhanvis235@gmail.com>
2026-09-22 00:46:47 +0530

    Update camera.js

--- src/camera.js
+++ src/camera.js
@@ -1,57 +1,43 @@
 export class CameraController {
-
   constructor(video) {
-
-    this.video =
-      video;
-
-    this.stream =
-      null;
+    this.video = video;
+    this.stream = null;
   }
 
-
   async start() {
+    if (!navigator.mediaDevices?.getUserMedia) {
+      throw new Error(
+        "Camera access is not available in this browser."
+      );
+    }
 
     this.stream =
       await navigator.mediaDevices.getUserMedia({
-
         video: {
-
           facingMode: "user",
-
           width: {
             ideal: 1280
           },
-
           height: {
             ideal: 720
           }
-
         },
-
         audio: false
-
       });
 
-
-    this.video.srcObject =
-      this.stream;
-
+    this.video.srcObject = this.stream;
 
     await this.video.play();
   }
 
-
   stop() {
+    if (this.stream) {
+      this.stream
+        .getTracks()
+        .forEach((track) => track.stop());
+    }
 
-    this.stream
-      ?.getTracks()
-      .forEach(
-        track => track.stop()
-      );
-
-    this.stream =
-      null;
+    this.stream = null;
+    this.video.srcObject = null;
   }
-
 }
